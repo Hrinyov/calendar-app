2b44433ae93ba2eee3e4da86a5393d586b156099
Hrynov <vh@controlledexplosion.net>
2024-03-13 17:15:38 +0200

    Move the drop context logic to calendar grid component

--- src/components/calendar-grid/calendar-grid.component.tsx
+++ src/components/calendar-grid/calendar-grid.component.tsx
@@ -3,8 +3,13 @@ import styled from "styled-components";
 import { CalendarDay } from "../calendar-cell/calendar-cell.component";
 import { generateCalendarDays, isToday } from "../../utils/calendarUtils";
 import { useAppDispatch } from "../../store/store";
-import { initDailyTaskArray } from "../../store/features/task-slice/task-slice";
+import {
+  initDailyTaskArray,
+  moveTaskOutArray,
+  moveTaskInArray,
+} from "../../store/features/task-slice/task-slice";
 import { Header } from "../calendar-grid-header/calendar-grid-header.component";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 const Grid = styled.div`
   display: grid;
@@ -22,19 +27,48 @@ export const CalendarGrid: FC = () => {
     const month = currentDate.getMonth() + 1;
     dispatch(initDailyTaskArray({ year, month }));
   }, [currentDate, dispatch]);
+
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (source.droppableId !== destination.droppableId) {
+      dispatch(
+        moveTaskOutArray({
+          taskId: result.draggableId,
+          sourceDate: source.droppableId,
+          destinationDate: destination.droppableId,
+        })
+      );
+    } else {
+      dispatch(
+        moveTaskInArray({
+          taskId: result.draggableId,
+          sourceDate: source.droppableId,
+          destinationDate: destination.droppableId,
+          destinationIndex: destination.index,
+        })
+      );
+    }
+  };
   return (
     <>
       <Header />
       <Grid>
-        {days.map((date) => {
-          return (
-            <CalendarDay
-              key={date.toISOString()}
-              date={date}
-              today={isToday(date)}
-            />
-          );
-        })}
+        <DragDropContext onDragEnd={onDragEnd}>
+          {days.map((date) => {
+            return (
+              <CalendarDay
+                key={date.toISOString()}
+                date={date}
+                today={isToday(date)}
+              />
+            );
+          })}
+        </DragDropContext>
       </Grid>
     </>
   );
--- src/components/task-list/task-list.component.tsx
+++ src/components/task-list/task-list.component.tsx
@@ -1,74 +1,48 @@
 import { FC } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+import { TaskListProps } from "./task-list.types";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 import { TaskItem } from "../task-item/task-item.component";
-import { useAppDispatch, useAppSelector } from "../../store/store"; 
-import { moveTaskInArray, moveTaskOutArray } from "../../store/features/task-slice/task-slice"; 
+import { useAppSelector } from "../../store/store";
 
-interface TaskListProps {
-  arrayKey: string;
-}
+const List = styled.div`
+  min-width: 150px;
+  min-height: 200px;
+  width: 100%;
+`;
 
 export const TaskList: FC<TaskListProps> = ({ arrayKey }) => {
-  const tasks = useAppSelector((state) => state.tasks[arrayKey] || []);
-  const dispatch = useAppDispatch();
-
-  const onDragEnd = (result) => {
-     if (!result.destination) {
-       if (result.reason === "DROP") {
-         dispatch(
-           moveTaskOutArray({
-             taskId: result.draggableId,
-             sourceDate: arrayKey,
-           })
-         );
-       }
-       return;
-     }
-     const items = Array.from(tasks);
-     const [reorderedItem] = items.splice(result.source.index, 1);
-     items.splice(result.destination.index, 0, reorderedItem);
-      if (result.destination.droppableId === "tasks") {
-        dispatch(
-          moveTaskInArray({
-            taskId: reorderedItem.id,
-            sourceDate: arrayKey,
-            destinationDate: arrayKey,
-            destinationIndex: result.destination.index,
-          })
-        );
-      }
-  };
+  const tasks = useAppSelector((state) => state.tasks[arrayKey]);
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="tasks">
-        {(provided) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
-            {tasks.map((task, index) => (
-              <Draggable
-                key={task.id}
-                draggableId={task.id.toString()}
-                index={index}
-              >
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    <TaskItem
-                      id={task.id}
-                      title={task.title}
-                      color={task.color}
-                    />
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <Droppable droppableId={arrayKey}>
+      {(provided) => (
+        <List {...provided.droppableProps} ref={provided.innerRef}>
+          {tasks?.map((task, index) => (
+            <Draggable
+              key={task.id}
+              draggableId={task.id.toString()}
+              index={index}
+            >
+              {(provided) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                >
+                  <TaskItem
+                    id={task.id}
+                    title={task.title}
+                    color={task.color}
+                    arrayKey={arrayKey}
+                  />
+                </div>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </List>
+      )}
+    </Droppable>
   );
 };
