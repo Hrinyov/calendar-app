4ae8b6538dac1627555ba1c3e445d3c793e6eed1
Hrynov <vh@controlledexplosion.net>
2024-03-13 17:19:28 +0200

    Refactor component to use AddTask component

--- src/components/calendar-cell/calendar-cell.component.tsx
+++ src/components/calendar-cell/calendar-cell.component.tsx
@@ -1,33 +1,48 @@
 import { FC } from "react";
 import styled from "styled-components";
+import { CalendarDayProps } from "./calendar-cell.types";
 import { TaskList } from "../task-list/task-list.component";
 import { transformDateToKey } from "../../utils/calendarUtils";
-
-interface CalendarDayProps {
-  date: Date;
-  today: boolean;
-}
+import { AddTask } from "../add-task/add-task.component";
 
 const DayCell = styled.div<{ istoday: boolean }>`
   border: 2px solid #a19d9d82;
+  border-radius: 10px;
   min-height: 100px;
   min-width: 100px;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
   align-items: start;
   padding: 10px;
   position: relative;
-  background-color: ${({ istoday }) => (istoday ? "#35b66288" : "#84a4b15f")};
-  color: ${({ istoday }) => (istoday ? "white" : "black")};
+  background-color: ${({ istoday }) => (istoday ? "#ffffffa5" : "#ffffff6f")};
+`;
+
+const DayHeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const HeaderItem = styled.span`
+  color: white;
+  display:inline;
+  font-weight: 700;
+  font-size: large;
 `;
 
 export const CalendarDay: FC<CalendarDayProps> = ({ date, today }) => {
-    
+  const dayKey = transformDateToKey(date);
   return (
     <DayCell istoday={today}>
-      {date.getDate()}
-      <TaskList arrayKey={transformDateToKey(date)} />
+      <DayHeader>
+        <HeaderItem>{date.getDate()}</HeaderItem>
+        <HeaderItem>
+          <AddTask dayKey={dayKey} />
+        </HeaderItem>
+      </DayHeader>
+      <TaskList arrayKey={dayKey} />
     </DayCell>
   );
 };
