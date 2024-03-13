baae72b9a21373287fb832c440c0e522e58ec87d
Hrynov <vh@controlledexplosion.net>
2024-03-13 17:02:43 +0200

    Add constants file

---
+++ src/utils/constants/constants.ts
@@ -0,0 +1,25 @@
+export const DAYS_OF_WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+export const MONTH_NAMES = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+export const COLORS = [
+  "gray",
+  "#e74c3c",
+  "#3498db",
+  "#50d35d",
+  "#e5eb38",
+  "orange",
+  "#a743a7",
+  "pink",
+];
