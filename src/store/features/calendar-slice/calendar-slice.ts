0531ab1ab9f450f62505ca7c7dce031cd5719ef0
Hrynov <vh@controlledexplosion.net>
2024-03-14 14:11:29 +0200

    Refactor calendar slice logic

--- src/store/features/calendar-slice/calendar-slice.ts
+++ src/store/features/calendar-slice/calendar-slice.ts
@@ -1,33 +1,33 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CalendarState } from "./calendar-slice.types";
 
-interface CalendarState {
-  currentDate: Date;
-}
+const currentDateStr = new Date().toISOString();
 
 const initialState: CalendarState = {
-  currentDate: new Date(), // Initializes to the current date, can be adjusted
+  currentDate: currentDateStr,
 };
 
 export const CalendarSlice = createSlice({
   name: "calendar",
   initialState,
   reducers: {
-    // Sets the current date, which can be used to adjust the viewed month/year
     setCurrentDate: (state, action: PayloadAction<Date>) => {
-      state.currentDate = action.payload;
+      const newDate = action.payload;
+      state.currentDate = newDate.toISOString();
     },
-    // Moves the current month viewed forward or backward
     adjustMonth: (state, action: PayloadAction<number>) => {
-      // action.payload is expected to be 1 for next month or -1 for previous month
-      const currentMonth = state.currentDate.getMonth();
-      state.currentDate.setMonth(currentMonth + action.payload);
+      const current = new Date(state.currentDate);
+      const adjustedMonth = new Date(
+        current.getFullYear(),
+        current.getMonth() + action.payload,
+        current.getDate()
+      );
+
+      state.currentDate = adjustedMonth.toISOString();
     },
-    // Additional reducers can be added here for other calendar actions
   },
 });
 
-// Export actions for use in components
 export const { setCurrentDate, adjustMonth } = CalendarSlice.actions;
 
-// Export the reducer as default export
 export default CalendarSlice.reducer;
